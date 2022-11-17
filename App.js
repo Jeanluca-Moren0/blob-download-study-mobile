fcc654bec4520d64e4d2b416258613face8b5fa9
Jeanluca-Moren0 <jeanlucam12@gmail.com>
2022-11-16 23:24:14 -0300

    fetched and saved file

--- App.js
+++ App.js
@@ -1,25 +1,79 @@
-
-import * as React from 'react';
+import { useState } from 'react';
 import { WebView } from 'react-native-webview';
+import { api } from './api';
+import * as FileSystem from 'expo-file-system';
+
 
 export default function App() {
+  const [id, setId] = useState('');
+  const [pdfBlob, setPdfBlob] = useState('');
+
   const jsCode = `
-      function downloadFile(obj){
-        alert("hello")
+        alert('start')
         const buttonContainer = document.getElementsByClassName('wrapper');
-        buttonContainer[0].children[1].onclick = function(){alert('hello')}
+        let idRequest;
+      
+        for(let i = 0; i <= buttonContainer.length; i++){
+
+          let idText = buttonContainer[i].children[0].innerText;
+
+          const ids =  idText.replace('Id do arquivo: ', '')
+          
+          let buttons = buttonContainer[i].children[1];
+          
+          buttons.onclick = () => {window.ReactNativeWebView.postMessage(ids)}; 
+        }
+  `;
+
+  const getPdfFile = async (id) => {
+    try {
+      const response = await api.get(`/pdf/${id}`, {
+        responseType: 'arraybuffer',
+      });
+
+      setPdfBlob(response.request._response);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+
+  const handleDowloadAndroid = async () => {
+    try {
+      const permissions =
+        await FileSystem.StorageAccessFramework.requestDirectoryPermissionsAsync();
+      if (!permissions.granted) {
+        return;
       }
 
-      downloadFile()
+      const fileName = 'PDF123';
+      const fileURI = await FileSystem.StorageAccessFramework.createFileAsync(
+        permissions.directoryUri,
+        fileName,
+        'application/pdf'
+      );
 
-  `;
+      await FileSystem.writeAsStringAsync(fileURI, pdfBlob, {
+        encoding: FileSystem.EncodingType.Base64,
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <WebView
+      forceDarkOn
+      incognito
       style={{ flex: 1 }}
       originWhitelist={['*']}
       source={{ uri: 'https://blob-download-study-frontend.vercel.app/' }}
-      forceDarkOn
-      incognito
+      onMessage={(event) => {
+        setId(event.nativeEvent.data);
+        getPdfFile(1);
+        handleDowloadAndroid();
+      }}
       javaScriptEnabledAndroid={true}
       injectedJavaScript={jsCode}
     />
